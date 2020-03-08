39756b579cd5abf11ed18661ebb3c5377cab9719
IonutTrisca <trisca46@gmail.com>
2020-03-08 21:16:49 +0200

    removed declaration and reading the stock from the loop

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include "VendingMachine.h"
 
 int main() {
-    //while(1) {
-        VendingMachine v;
-        v.readProducts();
+    VendingMachine v;
+    v.readProducts();
+
+    while(1) {
         v.showAvailableProducts();
         v.pickPaymentMethod();
-    //     sleep(10);
-    //     cout << "\033[2J\033[1;1H";
-    // }
+        sleep(10);
+
+        // clear the terminal output
+        cout << "\033[2J\033[1;1H";
+    }
+
     return 0;
 }
