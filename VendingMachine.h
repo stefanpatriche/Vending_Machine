6ab0d6a853d93282de7d83c54e2f7592d2c07689
IonutTrisca <trisca46@gmail.com>
2020-03-08 21:20:59 +0200

    Added class description and a method to check the card info

--- VendingMachine.h
+++ VendingMachine.h
@@ -3,20 +3,32 @@
 #ifndef _VENDING_MACHINE_H_
 #define _VENDING_MACHINE_H_
 
+/**
+ * This class simulates the behaviour of a vending machine.
+ * It can read the products from the input file available_products.txt.
+ * It accepts payment through both cash and credit card.
+ * It can return change at the end of a purchase for cash purchases.
+ */
 class VendingMachine {
-    vector<Product> availableProducts;
-    int currentPaymentMethod;            // 1 - card / 2 - cash
+    /**
+     * Vector which holds all the products
+     */
+    vector<Product> availableProducts;   
     int maxProductIndex;
     int minPrice;
     
 public:
     VendingMachine(){}
+    // Read the products from the available_products.txt file
     void readProducts();
     void showAvailableProducts();
     void pickPaymentMethod();
+
+private:
     void cardPayment();
     void cashPayment();
     bool checkStock(int);
+    bool checkCardInfo(string, string);
 };
 
 #endif
