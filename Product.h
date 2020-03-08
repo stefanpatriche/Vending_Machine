23cd97d8f1bf0ade3df523525963151b2185d3d8
IonutTrisca <trisca46@gmail.com>
2020-03-08 21:17:58 +0200

    Added class description

--- Product.h
+++ Product.h
@@ -8,12 +8,16 @@
 #include <algorithm>
 #include <unistd.h>
 
-
 using namespace std;
 
 #ifndef _PRODUCT_H_
 #define _PRODUCT_H_
 
+/**
+ * This class contains all the information relevant for a product
+ * in this vending machine. It hold the name, price and quantity left
+ * for a specific product.
+ */
 class Product {
     string name;
     int price;
